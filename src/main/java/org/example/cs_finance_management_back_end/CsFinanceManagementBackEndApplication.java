57cb4f6630247e22acc10c5e5dec174ff980418b
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-03-19 15:01:27 +0700

    first commit

---
+++ src/main/java/org/example/cs_finance_management_back_end/CsFinanceManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.cs_finance_management_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsFinanceManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsFinanceManagementBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cs_finance_management_back_end/CsFinanceManagementBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cs_finance_management_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsFinanceManagementBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
