89bee1c04c2fbc3b5d05b58477d5ddb05293f29e
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-03-20 16:50:33 +0700

    #8 change file and create crud walletdetails

---
+++ src/main/java/org/example/cs_finance_management_back_end/model/DTO/WalletdetailsForm.java
@@ -0,0 +1,2 @@
+package org.example.cs_finance_management_back_end.model.DTO;public class WalletdetailsForm {
+}
---
+++ src/main/java/org/example/cs_finance_management_back_end/service/IWalletdetailService.java
@@ -0,0 +1,2 @@
+package org.example.cs_finance_management_back_end.service;public interface IWalletdetailService {
+}
