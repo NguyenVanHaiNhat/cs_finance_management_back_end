31282c68c1e9ee7bdb809c2f27de7857de464090
vandat3012 <luongvandat301220@gmail.com>
2024-03-20 11:23:55 +0700

    #1 - build models of users and roles

--- src/main/java/org/example/cs_finance_management_back_end/model/Roles.java
+++ src/main/java/org/example/cs_finance_management_back_end/model/Roles.java
@@ -2,10 +2,12 @@ package org.example.cs_finance_management_back_end.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "role")
+@NoArgsConstructor
 public class Roles {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/example/cs_finance_management_back_end/model/Users.java
+++ src/main/java/org/example/cs_finance_management_back_end/model/Users.java
@@ -2,12 +2,14 @@ package org.example.cs_finance_management_back_end.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "user")
 @Data
+@NoArgsConstructor
 public class Users {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
