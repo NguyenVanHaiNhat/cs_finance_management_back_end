b17210e540e52b9896dd74e15b2e3d263d4a978a
vandat3012 <luongvandat301220@gmail.com>
2024-03-25 14:22:03 +0700

    #11 - validate with sign up

--- src/main/java/org/example/cs_finance_management_back_end/repository/IRolesRepository.java
+++ src/main/java/org/example/cs_finance_management_back_end/repository/IRolesRepository.java
@@ -3,7 +3,7 @@ package org.example.cs_finance_management_back_end.repository;
 import org.example.cs_finance_management_back_end.model.entity.Roles;
 import org.example.cs_finance_management_back_end.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.Set;
 
