56173a3bc8a6bbee88bacca2c199dc743df3cfa7
vandat3012 <luongvandat301220@gmail.com>
2024-03-25 14:20:32 +0700

    #11 - validate with sign up

--- src/main/java/org/example/cs_finance_management_back_end/repository/IRolesRepository.java
+++ src/main/java/org/example/cs_finance_management_back_end/repository/IRolesRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
-@Repository
+
 public interface IRolesRepository extends JpaRepository<Roles,Long> {
 }
